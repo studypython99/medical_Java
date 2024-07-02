a34c283f49ac62c512fecb92813e5a0d1527095b
My Name <fesz99@naver.com>
2024-07-02 11:07:47 +0900

    클래스

--- stuPjt/src/stuPjt/Stu_main.java
+++ stuPjt/src/stuPjt/Stu_main.java
@@ -16,10 +16,33 @@ public class Stu_main {
 		list.add(new Students("홍길자",100,100,99));
 		list.add(new Students("홍길순",100,100,99));
 		Stu_process sp = new Stu_process();
-		
+		https://muz.so/asKF
+
 		while (true)
 		{
+			int choice = sp.screen(list);
+			switch(choice)
+			{
+			case 1://학생입력
 			
+				break;
+			case 2://학생출력
+				
+				break;
+			case 3://학생수정
+				
+				break;
+			case 4://학생삭제
+				 
+				break;
+			case 5://학생검색
+				
+				break;
+			case 6://등수처리
+				
+				break;
+			
+			}//switch
 		}//while
 	}//main
 }//Stu_main
